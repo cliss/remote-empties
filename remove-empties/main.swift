281550a631dc902ad86e25e06bb1190ed215e9a2
Casey Liss <casey@caseyliss.com>
2020-06-02 20:26:10 -0400

    Initial Commit

---
+++ remove-empties/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  remove-empties
+//
+//  Created by Casey Liss on 2/6/20.
+//  Copyright © 2020 Casey Liss. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
